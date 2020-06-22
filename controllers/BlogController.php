d2146a17154e61c526b36482a535d7853c7fffa7
jandimar rocha <mazinhozr@gmail.com>
2020-06-22 10:38:04 -0300

    push

--- controllers/BlogController.php
+++ controllers/BlogController.php
@@ -22,15 +22,6 @@ class BlogController extends Controller
     public function behaviors()
     {
         return [
-            'access' => [
-                'class' => AccessControl::className(),
-                'rules' => [
-                    [
-                        'allow' => true,
-                        'roles' => ['@'],
-                    ],
-                ],
-            ],
             'verbs' => [
                 'class' => VerbFilter::className(),
                 'actions' => [
